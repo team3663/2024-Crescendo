94dc5a27a895489c7771ae1a9fd707d85910c1f2
LianaMendakoff <120145156+LianaMendakoff@users.noreply.github.com>
2024-01-29 20:59:23 -0800

    Worked on the basics of the subsystem shooter

---
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -0,0 +1,48 @@
+package frc.robot.subsystems.shooter;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
+
+public class Shooter extends SubsystemBase {
+    private final ShooterIO io;
+    private final ShooterIO.Inputs inputs = new ShooterIO.Inputs();
+
+    public Shooter(ShooterIO io) {
+        this.io = io;
+    }
+    @Override
+    public void periodic() {
+        io.updateInputs(inputs);
+        Logger.processInputs("Shooter", inputs);
+    }
+
+    public Command shootWithVoltage(double voltage) {
+        return runEnd(
+                () -> io.setVoltage(voltage),
+                () -> io.setVoltage(0.0)
+        );
+    }
+
+    public Command shootWithAngle(double angle) {
+        return runEnd(
+                () -> io.setAngle(angle),
+                () -> io.setAngle(0.0)
+        );
+    }
+
+    public Command shootIntoSpeaker(double voltage) {
+        return runEnd(
+                () -> io.setVoltage(voltage),
+                () -> io.setVoltage(0.0)
+        );
+    }
+
+    public Command shootIntoAmp(double voltage) {
+        return runEnd(
+                () -> io.setVoltage(voltage),
+                () -> io.setVoltage(0.0)
+        );
+    }
+
+}
---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
@@ -0,0 +1,33 @@
+package frc.robot.subsystems.shooter;
+
+import org.littletonrobotics.junction.LogTable;
+import org.littletonrobotics.junction.inputs.LoggableInputs;
+
+public interface ShooterIO {
+    default void updateInputs(Inputs inputs) {}
+    default void setVoltage(double voltage) {}
+    default void setAngle(double angle) {}
+
+    class Inputs implements LoggableInputs {
+        public double angularVelocity;
+        public double appliedVolts;
+        public double currentDrawAmps;
+        public double motorTemp;
+
+        @Override
+        public void toLog(LogTable table) {
+            table.put("AngularVelocity", angularVelocity);
+            table.put("AppliedVolts", appliedVolts);
+            table.put("CurrentDrawAmps", currentDrawAmps);
+            table.put("MotorTemp", motorTemp);
+        }
+
+        @Override
+        public void fromLog(LogTable table) {
+            angularVelocity = table.get("AngularVelocity", 0.0);
+            appliedVolts = table.get("AppliedVolts", 0.0);
+            currentDrawAmps = table.get("CurrentDrawAmps", 0.0);
+            motorTemp = table.get("MotorTemp", 0.0);
+        }
+    }
+}
