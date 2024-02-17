441f394d111d7222cc9fbeb2e58709b1fa7f8c61
Yhtomitos <67652876+Yhtomitos@users.noreply.github.com>
2024-02-17 09:53:08 -0800

    Expanded the robot factory to be able to create all subsystems

--- src/main/java/frc/robot/config/RobotFactory.java
+++ src/main/java/frc/robot/config/RobotFactory.java
@@ -1,10 +1,12 @@
 package frc.robot.config;
 
+import frc.robot.subsystems.climber.ClimberIo;
 import frc.robot.subsystems.drivetrain.DrivetrainIO;
+import frc.robot.subsystems.feeder.FeederIo;
 import frc.robot.subsystems.intake.IntakeIo;
 import frc.robot.subsystems.led.LedIo;
 import frc.robot.subsystems.pivot.PivotIo;
-import frc.robot.subsystems.led.LedIo;
+import frc.robot.subsystems.shooter.ShooterIo;
 
 public interface RobotFactory {
     default DrivetrainIO createDrivetrainIO() {
@@ -24,6 +26,21 @@ public interface RobotFactory {
 
     default PivotIo createPivotIo() {
         return new PivotIo() {
+        };
+    }
+
+    default ShooterIo createShooterIo() {
+        return new ShooterIo() {
+        };
+    }
+
+    default FeederIo createFeederIo() {
+        return new FeederIo() {
+        };
+    }
+
+    default ClimberIo createClimberIo() {
+        return new ClimberIo() {
         };
     }
 }
