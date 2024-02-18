f93d97456177de2e109a481ebfd9ddff6e5f93b9
Richard Elmore <richardelmore@live.com>
2024-02-17 16:39:09 -0800

    Clean constants and robot container and remove unused classes.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,7 +8,6 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -40,14 +39,13 @@ public class RobotContainer {
 
     private final CommandFactory commandFactory;
 
-    // Replace with CommandPS4Controller or CommandJoystick if needed
     private final CommandXboxController driverController =
             new CommandXboxController(DRIVER_CONTROLLER_PORT);
 
     private final SendableChooser<Command> autoChooser;
 
     /**
-     * The container for the robot. Contains subsystems, OI devices, and commands.
+     * The container for the robot. Contains subsystems, IO devices, and commands.
      */
     public RobotContainer(RobotFactory robotFactory) {
         climber = new Climber(robotFactory.createClimberIo());
@@ -79,7 +77,7 @@ public class RobotContainer {
                 .withSize(3, 1)
                 .withWidget(BuiltInWidgets.kComboBoxChooser);
     }
-//DriverTab
+
     /**
      * Use this method to define your trigger->command mappings. Triggers can be created via the
      * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
