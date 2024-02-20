3c6987cc46543b874e849c4fb4cd6c2809a4f5b7
Jesselyn Hong <jesselyn.hong.stu@cedarpark.org>
2024-02-19 21:22:53 -0800

    created Left and Right paths and autos

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
@@ -67,6 +68,9 @@ public class RobotContainer {
                         () -> -driverController.getRightX() * drivetrain.getConstants().maxRotationalVelocity()
                 )
         );
+
+        NamedCommands.registerCommand( "shootNote", shooter.shootCommand();
+        NamedCommands.registerCommand("intakeNote", intake.intakeCommand();
 
         // Configure the trigger bindings
         configureBindings();
