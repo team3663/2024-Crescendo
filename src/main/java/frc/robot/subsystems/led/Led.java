a90762b95e7092086671656c17b55c09e27a9cc6
LianaMendakoff <120145156+LianaMendakoff@users.noreply.github.com>
2024-02-29 21:33:30 -0800

    worked on getLedColor

--- src/main/java/frc/robot/subsystems/led/Led.java
+++ src/main/java/frc/robot/subsystems/led/Led.java
@@ -5,13 +5,11 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.utility.RobotMode;
 import org.littletonrobotics.junction.Logger;
 
+import static java.awt.Color.*;
+
 public class Led extends SubsystemBase {
     private final LedIo io;
-    private final LedColor black = new LedColor(0, 0, 0);
 
-    public final LedColor red = new LedColor(255,0,0);
-    public final LedColor yellow = new LedColor(255,255, 0);
-    public final LedColor blue = new LedColor(0,0,255);
 
     private final LedInputsAutoLogged inputs = new LedInputsAutoLogged();
 
@@ -34,6 +32,13 @@ public class Led extends SubsystemBase {
         return runEnd(
                 () -> io.setColor(color),
                 () -> io.setColor(black)
+        );
+    }
+
+    public Command defaultCommand() {
+        return run(
+                () -> setLedColor(red)
+
         );
     }
 }
--- src/main/java/frc/robot/utility/RobotMode.java
+++ src/main/java/frc/robot/utility/RobotMode.java
@@ -1,8 +1,15 @@
 package frc.robot.utility;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.led.LedColor;
 
 public class RobotMode {
+    public final LedColor red = new LedColor(255,0,0);
+    public final LedColor yellow = new LedColor(255,255, 0);
+    public final LedColor blue = new LedColor(0,0,255);
+    private final LedColor black = new LedColor(0, 0, 0);
+
+
     public enum Mode {
         SPEAKER,
         AMP,
@@ -21,6 +28,19 @@ public class RobotMode {
 
     public static Mode getMode() {
         return mode;
+    }
+
+    public LedColor getLedColor(Mode mode) {
+        switch(mode) {
+            case SPEAKER:
+                return blue;
+            case AMP:
+                return yellow;
+            case TRAP:
+                return red;
+            default:
+                return black;
+        }
     }
 
 }
